d5a376464187aaff1c3c3e47e18b405a92b0cd84
André Hopf <andre.hopf@gmail.com>
2023-01-18 17:08:41 -0300

    Adiciona login component

---
+++ src/app/pages/login-start/login-start.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-start',
+  templateUrl: './login-start.component.html',
+  styleUrls: ['./login-start.component.css']
+})
+export class LoginStartComponent {
+
+}
