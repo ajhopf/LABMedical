aac099fd1f514fe19c90304b5d0dbb52e1ffe3a9
André Hopf <andre.hopf@gmail.com>
2023-01-19 16:13:53 -0300

    Adiciona rota básica, e rota wildcard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginStartComponent } from "./pages/login-start/login-start.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginStartComponent
+  }, {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
