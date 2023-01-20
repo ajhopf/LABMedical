5023fd9b3ad7ab06b5faf7019230bcca779a8e26
André Hopf <andre.hopf@gmail.com>
2023-01-20 15:23:32 -0300

    Adiciona imagens, doctors service e layout inicial

--- src/app/pages/login-start/login-start.component.ts
+++ src/app/pages/login-start/login-start.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DoctorsDBService } from "../../services/doctors-db.service";
+import { URLS } from "../../constants/urls";
 
 @Component({
   selector: 'app-login-start',
   templateUrl: './login-start.component.html',
   styleUrls: ['./login-start.component.css']
 })
-export class LoginStartComponent {
+export class LoginStartComponent implements OnInit{
+  doctors: {email: string, password: string} | {} = {}
 
+  urls = URLS
+  password: string = '';
+  email: string = '';
+  constructor(
+    private doctorsDB: DoctorsDBService
+  ) {}
+
+  ngOnInit() {
+    console.log(this.urls)
+  }
 }
---
+++ src/app/services/doctors-db.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorsDBService {
+  BASE_URL = 'http://localhost:3000'
+  constructor(
+    private http: HttpClient
+  ) { }
+
+}
