ff82ff82c869d462b9eccfd2dd88d28ea5309628
André Hopf <andre.hopf@gmail.com>
2023-01-22 12:37:18 -0300

    Adiciona estrutura inicial para cadastro de pacientes

--- src/app/pages/pacient-registration/pacient-registration.component.ts
+++ src/app/pages/pacient-registration/pacient-registration.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pacient-registration',
   templateUrl: './pacient-registration.component.html',
   styleUrls: ['./pacient-registration.component.css']
 })
-export class PacientRegistrationComponent {
+export class PacientRegistrationComponent implements OnInit{
+  @ViewChild('newPacient') newPacientForm
+  @ViewChild('dob') dobInput
 
+  pacient = {
+    identification: {
+      pacientName: 'André',
+      pacientGender: 'masculino',
+      dob: new Date().toISOString().slice(0,10)
+    }
+  }
+
+  onCreatePacient(){
+    console.log(this.newPacientForm.value)
+  }
+
+  ngOnInit() {
+    console.log(this.pacient.identification.dob)
+  }
+
+  validateDob() {
+    console.log(this.dobInput.value)
+    return true
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
