11a8d069d8661989b082f1feb7a8b25b2625a880
André Hopf <andre.hopf@gmail.com>
2023-01-24 14:52:35 -0300

    Adiciona filtragem e seleção de pacientes e formulário

--- src/app/pages/appointment-registration/appointment-registration.component.ts
+++ src/app/pages/appointment-registration/appointment-registration.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PacientsDbService } from "../../services/pacients-db.service";
+import { FilterPacientsService } from "../../services/filter-pacients.service";
 
 @Component({
   selector: 'app-appointment-registration',
   templateUrl: './appointment-registration.component.html',
   styleUrls: ['./appointment-registration.component.css']
 })
-export class AppointmentRegistrationComponent {
+export class AppointmentRegistrationComponent implements OnInit{
+  pacients
+  filteredPacients
+  selectedPacient
+
+  appointment = {
+    pacientId: '',
+    reason: '',
+    date: new Date().toISOString().slice(0,10),
+    time: `${new Date().getHours()}:${new Date().getMinutes()}`,
+    description: '',
+    medication: '',
+    dosageAndPrecautions: ''
+  }
+
+  constructor(
+    private pacientsDB: PacientsDbService,
+    private filterPacientsService: FilterPacientsService
+  ) {}
+
+  ngOnInit(){
+    this.pacientsDB.getPacients().subscribe(
+      pacientsList => {
+        this.pacients = pacientsList;
+        //Retirar depois
+        // this.selectedPacient = this.pacients[0]
+        // console.log(this.appointment.time)
+      }
+    )
+  }
+
+  filterPacients(filter: string) {
+    if(filter) {
+      this.filteredPacients = this.filterPacientsService.filterPacients(this.pacients, filter)
+    } else {
+      this.filteredPacients = ''
+    }
+  }
+
+  onSelectPacient(pacient) {
+    console.log(pacient.id)
+
+    this.filteredPacients = ''
+    this.appointment.pacientId = pacient.id
+    this.selectedPacient = pacient
+  }
+
+  onFormSubmit(){
+    console.log(this.appointment)
+  }
 
 }
