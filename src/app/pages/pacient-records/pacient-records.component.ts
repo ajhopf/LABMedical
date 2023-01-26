a82d60ca45f53303f0cd47fe86d03266a2f1315e
André Hopf <andre.hopf@gmail.com>
2023-01-26 14:53:23 -0300

    Adiciona interface para consulta, exame e paciente. Busca registros de acordo com o paciente

--- src/app/pages/pacient-records/pacient-records.component.ts
+++ src/app/pages/pacient-records/pacient-records.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
+
+import { PacientsDbService } from "../../shared/services/pacients-db.service";
+import { AppointmentsDbService } from "../../shared/services/appointments-db.service";
+import { ExamsDbService } from "../../shared/services/exams-db.service";
+import { Pacient } from "../../shared/models/pacient.model";
+import { Appointment } from "../../shared/models/appointment.model";
+import { Exam } from "../../shared/models/exam.model";
 
 @Component({
   selector: 'app-pacient-records',
   templateUrl: './pacient-records.component.html',
   styleUrls: ['./pacient-records.component.css']
 })
-export class PacientRecordsComponent {
+export class PacientRecordsComponent implements OnInit {
+  userId: string
+  pacient: Pacient
+  pacientsAppointments: Appointment[] = []
+  pacientsExams: Exam[] = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private pacientsDB: PacientsDbService,
+    private appointmentsDB: AppointmentsDbService,
+    private examsDB: ExamsDbService
+  ) {
+  }
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.params['id']
+
+    if (this.userId) {
+      this.pacientsDB.getPacient(this.userId).subscribe((pacient: Pacient) => {
+        this.pacient = pacient
+        console.log(this.pacient)
+      })
+    }
+
+    this.appointmentsDB.getAppointments().subscribe(
+      (appointments: Appointment[]) => {
+        let pacientAppointments = appointments.filter(appointment => appointment['pacientId'] === +this.userId)
+        this.pacientsAppointments = [...pacientAppointments]
+        console.log(this.pacientsAppointments)
+      }
+    )
+
+    this.examsDB.getExams().subscribe(
+      (exams: Exam[]) => {
+        let pacientExams = exams.filter(exam => exam['pacientId'] === +this.userId)
+        this.pacientsExams = [...pacientExams]
+        console.log(this.pacientsExams)
+      }
+    )
+  }
 }
